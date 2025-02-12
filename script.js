64cf8db0b3320b7c068d0b35120e0628e6bdbbfb
Omrajakhadka <omrajakhadka7@gmail.com>
2025-02-12 19:13:03 +0545

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) (
+    event.preventDefault();
+
+    const name = document.getElementById('name'),value;
+    const email = document.getElementById('email'),val;
+    const message = document.getElementById('message'),v;
+
+    alert('Thank You, ${name}! Your message has been recieved');
+)
+
